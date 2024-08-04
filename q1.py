c458df04b7a5076a17b76a3e124d31a1b83377dd
megan5782 <megan6u5892@gmail.com>
2024-08-04 14:15:16 +0800

    新增分支在test佔放

--- q1.py
+++ q1.py
@@ -8,4 +8,9 @@ for w in range(5):
         print("猜對了")
         break
     else:
-        print("猜錯了 ~~~~")
+        if x > y:
+            print("猜高一點")
+        else:
+            print("猜錯了 ~~~~")
+if x != y:
+    print(f"答案為:{x}")
